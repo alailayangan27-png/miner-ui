4a0cb214dfca403659e8df4f9e945a902782f580
alailayangan27-png <alailayangan27@gmail.com>
2026-04-17 12:24:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
 
-const API = "https://mining-production-ad0d.up.railway.app";
-const RECEIVER = "UQAPRU6cHYSkS8hIxl-zbcts9yt8_GtYcSh_R0nbYnWL5lFX"; // wallet TON kamu
+const API = "https://ISI-LINK-RAILWAY";
+const RECEIVER = "ISI-WALLET-TON-KAMU";
 
 export default function App() {
   const [tonConnectUI] = useTonConnectUI();
@@ -10,14 +10,12 @@ export default function App() {
 
   const user_id = "user1";
 
-  // ambil data user
   const load = async () => {
     const res = await fetch(API + "/user", {
       method: "POST",
       headers: {"Content-Type":"application/json"},
       body: JSON.stringify({user_id})
     });
-
     const data = await res.json();
     setTotal(data.total);
   };
@@ -26,7 +24,6 @@ export default function App() {
     load();
   }, []);
 
-  // fungsi mint (kirim TON)
   const mint = async () => {
     try {
       await tonConnectUI.sendTransaction({
@@ -34,12 +31,11 @@ export default function App() {
         messages: [
           {
             address: RECEIVER,
-            amount: "100000000" // 0.1 TON
+            amount: "100000000"
           }
         ]
       });
 
-      // setelah kirim → catat ke backend
       await fetch(API + "/deposit", {
         method: "POST",
         headers: {"Content-Type":"application/json"},
@@ -52,7 +48,7 @@ export default function App() {
       load();
 
     } catch (e) {
-      console.log("cancel / error");
+      console.log("cancel");
     }
   };
 
@@ -70,18 +66,8 @@ export default function App() {
       <h1>Total Deposit</h1>
       <h2>{total} TON</h2>
 
-      <button
-        onClick={mint}
-        style={{
-          marginTop:"40px",
-          padding:"15px 30px",
-          background:"#00f2ff",
-          border:"none",
-          borderRadius:"10px",
-          fontSize:"18px"
-        }}
-      >
-        Mint (0.1 TON)
+      <button onClick={mint}>
+        Mint 0.1 TON
       </button>
 
     </div>
