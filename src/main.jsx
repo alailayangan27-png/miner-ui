bfa35d20999297867911b1b4643a9a091733a4c4
alailayangan27-png <alailayangan27@gmail.com>
2026-04-17 12:30:41 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <TonConnectUIProvider manifestUrl="https://NAMA-APP.vercel.app/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://mining-production-ad0d.up.railway.app/tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>
 );
